388e344d8efef5fe63051f97caf8cdc54499e9fc
goodaccoustics <@Firehouse1>
2019-10-31 07:34:03 +0800

    dev commit

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route} from "react-router-dom";
 import { store } from '../store'
 import { ConnectedDashBoard } from "./Dashboard";
+import { ConnectedNavigation } from "./Navigation";
+import { ConnectedTaskDetail } from "./TaskDetail";
+import { history } from "../store/history";
+
 
 export const Main = () => (
-  <Provider store={ store } >
-    <div>
-      <ConnectedDashBoard />
-    </div>
-  </Provider>
+  <Router history={history}>
+    <Provider store={ store } >
+      <div>
+        <ConnectedNavigation />
+        <Route
+          exact
+          path="/dashboard"
+          render={() => (
+            <ConnectedDashBoard />
+          )}
+        />
+        <Route
+          exact
+          path="/task/:id"
+          render={({match}) => (
+              <ConnectedTaskDetail match={match} />
+          )}
+        />
+      </div>
+    </Provider>
+  </Router>
+
 );
--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { requestTaskCreation } from "../store/mutations";
+import { Link } from "react-router-dom";
 
 const mapStateToProps = (state, ownProps) => {
   let groupId= ownProps.id;
@@ -10,20 +12,38 @@ const mapStateToProps = (state, ownProps) => {
   }
 };
 
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createNewTask(id) {
+      console.log("Creating new task ... ");
+      dispatch(requestTaskCreation(id));
+    }
+  }
+};
 
-export const TaskList = ({name, tasks}) => (
+
+export const TaskList = ({name, tasks, id, createNewTask}) => (
   <div>
-    <h3>{name}</h3>
+    <h3>
+      {name}
+    </h3>
     <div>
       {tasks.map(task => (
-        <div>
-          { task.name }
+        <div key={ task.id }>
+          <Link to={`/task/${task.id}`}>
+            { task.name }
+          </Link>
         </div>
       ))}
     </div>
+    <button onClick={ () =>
+      createNewTask(id) }
+    >
+      Add New
+    </button>
   </div>
 );
 
 
 
-export const ConnectedTaskList = connect(mapStateToProps)(TaskList);
+export const ConnectedTaskList = connect(mapStateToProps, mapDispatchToProps)(TaskList);
